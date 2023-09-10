0944ead817ffb796fa1cb8f3820955dcb79c8c2e
muhfrds345@gmail.com <muhfrds345@gmail.com>
2023-09-10 22:10:23 +0700

    Initial commit is add Thread and how to use that flexible running with add Executor and Handler

---
+++ app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.dicoding.mybackgroundthread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val tvStatus = findViewById<TextView>(R.id.tv_status)
+
+        //Jika data delay nya banyak harus menggunakan Executor
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+
+        btnStart.setOnClickListener {
+            executor.execute {
+                try {
+                    //simulate process compressing
+                    for (i in 0..10) {
+                        Thread.sleep(500)
+                        val percentage = i * 10
+                        handler.post{
+                            //update UI in main Thread
+                            if (percentage == 100) {
+                                tvStatus.setText(R.string.task_completed)
+                            } else {
+                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                            }
+                        }
+                    }
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+
+            }
+
+        }
+    }
+}
